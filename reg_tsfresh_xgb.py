dce9b8fb5f78618ffcee3ade465006d5f4904f95
Joachim Schaeffer <joachim@Joachims-MacBook-Pro-3.local>
2022-10-11 09:19:16 -0400

    regression task added

--- reg_tsfresh_xgb.py
+++ reg_tsfresh_xgb.py
@@ -6,256 +6,209 @@ from joblib import load
 import datetime
 import os
 
-from sklearn.pipeline import Pipeline
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.compose import TransformedTargetRegressor
 from sklearn.preprocessing import QuantileTransformer
+from sklearn.dummy import DummyRegressor
+from sklearn.metrics import mean_absolute_error
 
 import xgboost as xgb
-
-from tsfresh.transformers import RelevantFeatureAugmenter
-from tsfresh.transformers import FeatureAugmenter
-from tsfresh.feature_extraction import ComprehensiveFCParameters
 
 from utils_preprocessing import preprocess_data
 from utils_preprocessing import process_batch_element_params_str 
 from utils_preprocessing import process_batch_element_params
-from utils_preprocessing import eis_label_encoder
 from preprocess import unwrap_df
  
 from clf_tsfresh_xgb import load_features_le
 
-
-def create_pipeline_reg(df_ts_):
-    """Creates the pipeline for the regression model.
-    
-    Parameters
-    ----------
-    df_ts_ : pandas.DataFrame
-        The dataframe containing the unwrapped data for the given ECM type.
-    
-    Returns
-    -------
-    pipe : sklearn.pipeline.Pipeline
-        The pipeline for the regression model.
-    """
-    mdl = MultiOutputRegressor(estimator=xgb.XGBRegressor())
-    regr = TransformedTargetRegressor(
-        regressor=mdl,
-        transformer=QuantileTransformer(n_quantiles=10, random_state=42)
-    )
-    pipe = Pipeline(
-        [
-            (
-                "augmenter",
-                FeatureAugmenter(
-                    column_id="id",
-                    column_sort="freq",
-                    default_fc_parameters=ComprehensiveFCParameters(),
-                ),
-            ),
-            ("regressor", regr),
-        ]
-    )
-    pipe.set_params(augmenter__timeseries_container=df_ts_)
-    return pipe
 
 def write_param_str_factory(param_strs):
     def write_param_str(param_values):
         return ", ".join([f"{key}: {value}" for key, value in zip(param_strs, param_values)])
     return write_param_str
 
-def fit_ecm_parameter_model(df, df_ts, output_dir):
+### Try again with saved features
+def fit_ecm_parameter_model(df, df_test, quantile_transformer, use_dummy_model, output_dir):
     """Fits the ECM parameter model for each ECM type.
+    loads features directly from file"""
+        
+    # Select regressor
+    if not use_dummy_model:
+        mdl = MultiOutputRegressor(estimator=xgb.XGBRegressor())
+        model_name = "xgb-ts-feat"
+    else:
+        mdl = DummyRegressor(strategy='median')
+        model_name = "dummy"
 
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        The dataframe containing the raw data.
-    df_ts : pandas.DataFrame
-        The dataframe containing the unwrapped data.
-    output_dir : str    
-        The directory to save the model to.
-
-    Returns
-    -------
-    None
-    """
-    for ecm in df["Circuit"].unique():
-        print(f"Fitting regression for {ecm}")
-
-        ## Subselect circuits of given ECM
-        df_ecm = df[df["Circuit"] == ecm]
-        df_ts_ = df_ts[df_ts["id"].isin(df_ecm.index)]
-
-        df_x = pd.DataFrame(index=np.unique(df_ts_["id"]))
-        df_y = pd.DataFrame(
-            df_ecm["param_values"].to_list(),
-            columns=df_ecm["param_strs"].loc[df_ecm.index[0]]
-        )
-
-        print("Starting regr. feature extraction and training pipeline")
-        pipe = create_pipeline_reg(df_ts_)
-        ## Evaluate features and fit XGBoost Model
-        pipe.fit(df_x, df_y)
-
-        print("trainign completed, writing model and predictions to disc")
-        dump(pipe, f"{output_dir}/xgb-ts-feat-reg-{ecm}-data-pipeline.joblib")
-
-        with open(f"{output_dir}/{ecm}_param-names.txt", "w") as f:
-            json.dump(df_ecm["param_strs"].loc[df_ecm.index[0]].tolist(), f)
-
-    return 
-
-def predict_ecm_parameters(df, df_ts, clf_model, X, label_dict, output_dir, train): 
-    """Predicts the ECM parameters for the given data and model. 
-    Saves the predictions to a file.
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        The dataframe containing the raw data.
-    df_ts : pandas.DataFrame
-        The dataframe containing the unwrapped data.
-    clf_model : xgboost.XGBClassifier
-        The trained model.
-    X : ndarray   
-        The feature matrix.
-    label_dict : dict
-        The dictionary containing the label mapping.
-    output_dir : str
-        The directory to save the predictions to.
-    train : bool
-        Whether the data is from the training set.
+    if quantile_transformer:
+        regr = TransformedTargetRegressor(
+            regressor=mdl,
+            transformer=QuantileTransformer(n_quantiles=10, random_state=0)
+            )
+        model_name += "-qt"
+    else:
+        regr = mdl
     
-    Returns
-    -------
-    df : pandas.DataFrame
-        The dataframe containing the raw data and the predictions.
-    """
+    le_path = 'data/le_name_mapping.json'
+    # Creating labels y
+    with open(le_path, 'r') as f:
+        mapping = json.load(f)
+    mapping['classes'] = [mapping[str(int(i))] for i in range(9)]
+    df["param_values_pred"] = [""] * len(df)
+    df_test["param_values_pred"] = [""] * len(df_test)
 
-    print("Predict class")
-    df["pred_label"] = clf_model.predict(X)
-    df["pred_circuit"] = df["pred_label"].apply(int).apply(str).map(label_dict)
+    for ecm in mapping['classes']:
+        print(f"ECM: {ecm}, Model: {model_name}")
+        # Load features (due to nans in the data loadtxt or genfromtxt might fail
+        try:
+            train_data = np.loadtxt(f"data/reg-tsfresh/{ecm}_data_train.csv")
+        except:
+            train_data = np.genfromtxt(f"data/reg-tsfresh/{ecm}_data_train.csv", delimiter=",")
+        try:
+            test_data = np.loadtxt(f"data/reg-tsfresh/{ecm}_data_test.csv")
+        except:
+            test_data = np.genfromtxt(f"data/reg-tsfresh/{ecm}_data_test.csv", delimiter=",")
 
-    df["Parameters"] = [""] * len(df)
-
-    for ecm in label_dict.values():
-        print(f"Predict model params for {ecm}")
-        reg_pipe = load(f"{output_dir}/xgb-ts-feat-reg-{ecm}-data-pipeline.joblib")
-
-        with open(f"{output_dir}/{ecm}_param-names.txt", "r") as f:
+        # Load param names from json file
+        with open(f"data/reg-tsfresh/{ecm}_param_names.json", 'r') as f:
             param_strs = json.load(f)
 
-        df_ecm = df[df["pred_circuit"] == ecm]
+        nb_params = len(param_strs)
+        # Preprocess data
+        X_train = train_data[:,0:-nb_params]
+        y_train = train_data[:,-nb_params:]
+        X_test = test_data[:,0:-nb_params]
+        y_test = test_data[:,-nb_params:]
 
-        if len(df_ecm) == 0:
-            continue
+        # Train model
+        regr.fit(X_train, y_train)
+        # Save model
+        dump(regr, f"{output_dir}/models/{model_name}-reg-{ecm}-reg-model.joblib")
 
-        df_ts_ = df_ts[df_ts["id"].isin(df_ecm.index)]
+        # Make predictions on train set 
+        pred_values_train = regr.predict(X_train)
+        # Make predictions on test set
+        pred_values_test = regr.predict(X_test)
 
-        df_x = pd.DataFrame(index=np.unique(df_ts_["id"]))
+        df_ecm = df[df["Circuit"] == ecm]
+        df.loc[df_ecm.index, "param_values_pred"] = [[pred_values_train[i, :]] for i in range(pred_values_train.shape[0])]
+        df_test_ecm = df_test[df_test["Circuit"] == ecm]
+        df_test.loc[df_test_ecm.index, "param_values_pred"] = [[pred_values_test[i, :]] for i in range(pred_values_test.shape[0])]
 
-        reg_pipe.set_params(augmenter__timeseries_container=df_ts)
-        pred_values = reg_pipe.predict(df_x)
+        # print('Save predictions')
+        # np.savetxt(pred_values_train, f"{output_dir}/predictions/{model_name}_{ecm}_train_data_predictions.csv")
+        # np.savetxt(pred_values_test, f"{output_dir}/predictions/{model_name}_{ecm}_test_data_predictions.csv")
 
-        f = write_param_str_factory(param_strs)
+    path_train = f"{output_dir}/predictions/{model_name}_{ecm}_train_data_predictions.pkl"
+    path_test = f"{output_dir}/predictions/{model_name}_test_data_predictions.pkl"
+    df.to_pickle(path_train)
+    df_test.to_pickle(path_test)
 
-        df.loc[df_ecm.index, "pred_parameters"] = [f(params) for params in pred_values]
+    score_qs_vs_pred(path_train, path_test, model_name)
+    return
 
-    print('Save predictions')
-    if train:
-        df[["pred_circuit", "pred_parameters"]].to_csv(f"{output_dir}/train_data_predictions.csv")
-    else:
-        df[["pred_circuit", "pred_parameters"]].to_csv(f"{output_dir}/test_data_predictions.csv")
+def score_qs_vs_pred(path_train, path_test, model_name):
+    # f = write_param_str_factory(param_strs)
+    # pred_parameters_train = [f(params) for params in pred_values_train]
+    # pred_parameters_test = [f(params) for params in pred_values_test]
+    df = pd.read_pickle(path_train)
+    df_test = pd.read_pickle(path_test)
+    df_results = pd.DataFrame()
+    results = []
+    for ecm in np.unique(df['Circuit']):
+        df_ecm = df[df['Circuit'] == ecm]
+        df_test_ecm = df_test[df_test['Circuit'] == ecm]
 
-    return df
+        y_true_train = np.stack(df_ecm['param_values'].values, axis=0).squeeze()
+        y_pred_train = np.stack(df_ecm['param_values_pred'].values, axis=0).squeeze()
+        y_true_test = np.stack(df_test_ecm['param_values'].values, axis=0).squeeze()
+        y_pred_test = np.stack(df_test_ecm['param_values_pred'].values, axis=0).squeeze()
 
-def dummy_model(): 
-    # dummy model, no quantile transform
-    ppls_0 = list()
-    for df_ in dfs:
-        print(" ")
-        print("Fitting regression for %s" % df_["Circuit"].loc[df_.index[0]])
-        df_ts_ = df_ts[df_ts["id"].isin(df_.index)]
-        df_x = pd.DataFrame(index=np.unique(df_ts_["id"]))
-        df_y = pd.DataFrame(df_['param_values'].to_list(), columns=df_["param_strs"].loc[df_.index[0]])
-        X_train, X_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=42)
-        # transformer mask, tell the transformer which parameters to skip over
-        mask = [n for n, x in enumerate(df_["param_strs"].loc[df_.index[0]]) if '_t' in x]
-        mdl = DummyRegressor(strategy='median')
-        if quantile: 
-              regr = TransformedTargetRegressor(regressor=mdl,
-                                    transformer=QuantileTransformer(n_quantiles=10, random_state=0))
-            #regr = TransformedTargetRegressor(regressor=mdl, 
-            #                                  func=partial(transform_params, mask_skip=mask),
-            #                                  inverse_func=partial(invert_params, mask_skip=mask))
-        else:
-            regr = mdl
-        ppl = Pipeline([
-                ('regressor', regr)
-            ])
+        mae_train = MAE_loss(y_true_train, y_pred_train)
+        mae_test = MAE_loss(y_true_test, y_pred_test)
 
-        ppl.fit(X_train, y_train)
-        y_pred = ppl.predict(X_test)
-        print("MAE:%.4g" % mean_absolute_error(y_test, y_pred))
-        p_strs = df_["param_strs"].loc[df_.index[0]]
-        for i in np.arange(len(p_strs)):
-            print("%s MAE:%.4g" % (p_strs[i], mean_absolute_error(y_test[p_strs[i]], y_pred[:,i])))
-        ppls_0.append(ppl)
-    return None
+        print(f"ECM: {ecm}, MAE train: {mae_train}, MAE test: {mae_test}")
+        for i, element in enumerate(df_ecm['param_strs'].iloc[0]):
+            # mae_train_element = MAE_loss(y_true_train.apply(lambda x: x[i]), y_pred_train.apply(lambda x: x[i]))
+            mae_test_element = MAE_loss(y_true_test[:, i], y_pred_test[:, i])
+            res_element = [ecm, element, mae_test_element]
+            results.append(res_element)
+    # Save results
+    df_results = pd.DataFrame(results, columns=['ecm', 'param', 'mae'])
+    df_results.to_csv(f"{output_dir}/predictions/{model_name}_qs_vs_pres_mae_results.csv", index=False)
+    return results
+
+def score_eis_custom_cv_loss(path_train, path_test): 
+    pass
+    return
 
 def MAE_loss(y_true, y_pred):
     return np.mean(np.abs(y_true - y_pred))
 
 
 if __name__ == "__main__":
+    train_pred = True
 
-    le_f = "data/le_name_mapping.json"
-    train_data_fname = 'data/train_data.csv'
-    test_data_fname = 'data/test_data.csv'
-    clf_model_fname = 'results/clf/xgb/2022-09-30_11-59-51/model.joblib'
+    if train_pred:
+        le_f = "data/le_name_mapping.json"
+        train_data_fname = 'data/train_data.csv'
+        test_data_fname = 'data/test_data.csv'
+        clf_model_fname = 'results/clf/xgb/2022-09-30_11-59-51/model.joblib'
 
-    # Load tsfresh data directly from file
-    train_data_f = "data/train_tsfresh.csv"
-    test_data_f = "data/test_tsfresh.csv"  
-    Xtsf_train, ytsf_train, Xtsf_test, ytsf_test, le = load_features_le(train_data_f, test_data_f, le_f)
+        # Load tsfresh data directly from file
+        train_data_f = "data/train_tsfresh.csv"
+        test_data_f = "data/test_tsfresh.csv"  
+        Xtsf_train, ytsf_train, Xtsf_test, ytsf_test, le = load_features_le(train_data_f, test_data_f, le_f)
 
-    now = datetime.datetime.now()
-    now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
-    output_dir = f"results/reg/xgb/{now_str}"
-    os.mkdir(output_dir)
+        now = datetime.datetime.now()
+        now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
+        output_dir = f"results/reg/xgb/{now_str}"
+        os.mkdir(output_dir)
+        os.mkdir(output_dir+"/models")
+        os.mkdir(output_dir+"/predictions")
 
-    df = preprocess_data(train_data_fname)
-    df_test = preprocess_data(test_data_fname)
+        df = preprocess_data(train_data_fname)
+        df_test = preprocess_data(test_data_fname)
 
-    df_ts = unwrap_df(df)
-    df_ts_test = unwrap_df(df_test)
+        df_ts = unwrap_df(df)
+        df_ts_test = unwrap_df(df_test)
 
-    df["param_strs"] = df.apply(
-        lambda x: process_batch_element_params_str(x.Parameters), axis=1
-    )
-    df["param_values"] = df.apply(
-        lambda x: process_batch_element_params(x.Parameters), axis=1
-    )
+        df["param_strs"] = df.apply(
+            lambda x: process_batch_element_params_str(x.Parameters), axis=1
+        )
+        df["param_values"] = df.apply(
+            lambda x: process_batch_element_params(x.Parameters), axis=1
+        )
 
-    df_test["param_strs"] = df_test.apply(
-        lambda x: process_batch_element_params_str(x.Parameters), axis=1
-    )
-    df_test["param_values"] = df_test.apply(
-        lambda x: process_batch_element_params(x.Parameters), axis=1
-    )
-    
-    fit_ecm_parameter_model(df, df_ts, output_dir)
+        df_test["param_strs"] = df_test.apply(
+            lambda x: process_batch_element_params_str(x.Parameters), axis=1
+        )
+        df_test["param_values"] = df_test.apply(
+            lambda x: process_batch_element_params(x.Parameters), axis=1
+        )
+        # Fit and predict and save results
+        fit_ecm_parameter_model(
+            df, df_test, 
+            quantile_transformer=False, use_dummy_model=False, 
+            output_dir=output_dir)
+        
+        fit_ecm_parameter_model(
+            df, df_test, 
+            quantile_transformer=True, use_dummy_model=False, 
+            output_dir=output_dir)
+        
+        fit_ecm_parameter_model(
+            df, df_test, 
+            quantile_transformer=False, use_dummy_model=True, 
+            output_dir=output_dir)
 
-    print("Load clf model")
-    le, label_dict = eis_label_encoder(le_f)
-    del(label_dict['classes'])  # Delete the classes key
 
-    clf_model = load(clf_model_fname)
+    #if score:
+    #    # Score predictions
+    #    path_train = 'results/reg/xgb/2022-10-04_20-03-55/predictions/xgb-ts-feat_train_data_predictions.pkl'
+    #    path_test = 'results/reg/xgb/2022-10-04_20-03-55/predictions/xgb-ts-feat_test_data_predictions.pkl'
+    #    results = score_qs_vs_pred(path_train, path_test)
 
-    # predict_ecm_parameters(df, df_ts, clf_model, Xtsf_train, label_dict, output_dir, train=True)
-    predict_ecm_parameters(df_test, df_ts_test, clf_model, Xtsf_test, label_dict, output_dir, train=False)
+    # Score predictions with local eis package custom scoring function
 
-    mae
     print("Done")
